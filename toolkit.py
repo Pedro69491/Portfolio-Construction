da9f94663a0eb8c8ab341ff590fec3f4aeb3a7da
Pedro69491 <pnrmc@outlook.pt>
2022-07-14 19:43:15 +0100

    ef.plot

--- toolkit.py
+++ toolkit.py
@@ -2,6 +2,7 @@ import pandas as pd
 import scipy.stats
 import numpy as np
 from scipy.stats import norm
+from scipy.optimize import minimize
 
 def preprocess_returns(file_path):
     """preprocessing csv file:
@@ -15,6 +16,19 @@ def preprocess_returns(file_path):
 
 def monthly_return(df):
     return np.prod(1+ df)**(1/df.shape[0]) -1
+
+def annualized_return(rets, periods_per_year):
+    number_periods = rets.shape[0]
+    return np.prod(1+rets)**(periods_per_year/number_periods) - 1
+
+def annualized_volatility(rets):
+    number_periods= rets.shape[0]
+    return rets.std()*(number_periods**0.5)
+
+def sharpe_ratio(rets, rfr, periods_per_year):
+    r = annualized_return(rets, periods_per_year)
+    v = annualized_volatility(rets)
+    return (r-rfr)/v
 
 def drawdown(df, init_wealth):
     wealth_index = init_wealth*(1+df).cumprod()
@@ -56,7 +70,7 @@ def historic_cVaR(r,level):
     else:
         raise TypeError('not a dataframe or series')
         
-def gaussian_VaR(r, level, cornish=False):
+def gaussian_VaR(r, level=5, cornish=False):
     s, k = skewness_kurtosis(r)
     z = norm.ppf(level/100)
     if cornish:
@@ -66,3 +80,58 @@ def gaussian_VaR(r, level, cornish=False):
                 (2*z**3 - 5*z)*(s**2)/36
             )
     return -(r.mean() + z*r.std(ddof=0))
+
+def portfolio_return(weights, er):
+    return np.matmul(weights.T, er)
+
+def portfolio_volatility(weights, cov):
+    return (weights.T @ cov@ weights)**0.5
+
+def plot_ef_2(er, cov, n_points):
+    if cov.shape[0] > 2:
+        raise TypeError('More than two assets')
+        
+    weights = [np.array((w,1-w)) for w in np.linspace(0,1,n_points)]
+    r = [tk.portfolio_return(w, er) for w in weights]
+    v = [tk.portfolio_volatility(w, er) for w in weights]
+    ef = pd.DataFrame({'Return':r, 'Volatility':v})
+    return ef.plot.scatter(x='Volatility', y='Return')
+
+def minimize_vol(target_return, cov, er):
+    
+    n = cov.shape[0]
+    bounds = ((0.0,1.0),)*n
+    init_weights = np.repeat(1/n, n)
+    r_to_target = {
+        'type': 'eq',
+        'args': (er,),
+        'fun': lambda er, weights: target_return - portfolio_return(weights, er)
+    }
+    w_to_1 = {
+        'type': 'eq',
+        'fun': lambda weights: np.sum(weights) - 1
+    }
+    
+    weights = minimize(portfolio_volatility, 
+                   init_weights, args=(cov,), 
+                   method='SLSQP', 
+                   options={'disp':False}, 
+                   bounds=bounds, 
+                   constraints=(r_to_target, w_to_1)
+                   )
+    
+    return weights.x
+
+def optimal_weights(cov, er, n_points):
+    t_r = np.linspace(er.min(), er.max(), n_points)
+    weights = [minimize_vol(r, cov, er) for r in t_r]
+    return weights
+
+def plot_ef(cov, er, n_points):
+    weights = optimal_weights(cov, er, n_points)
+    r = [portfolio_return(w, er) for w in weights]
+    v = [portfolio_volatility(w, cov) for w in weights]
+    ef = pd.DataFrame({'Return':r, 'Volatility':v})
+    return ef.plot.line(x='Volatility', y='Return', style='.-')
+    
+    
